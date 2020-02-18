dba8b7e69d10aee07bf37f92bf450a3ee4aaa5a9
Darya Kuliashova <dk@bam-boo.eu>
2020-02-18 14:03:38 +0300

    initial commit

---
+++ State and Binding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  State and Binding
+//
+//  Created by Developer on 18.02.2020.
+//  Copyright © 2020 Darya Kuliashova. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
